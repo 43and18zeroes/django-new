d95e6e5851327b5a5bc9fbed7af66be4ced5539e
Christoph <c.wagner.germany@gmail.com>
2024-10-06 15:08:14 +0200

    json7

--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect
 from django.http import HttpResponse
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotFound, Http404
 from django.utils.text import slugify
 from django.urls import reverse
 
@@ -12,15 +12,20 @@ def start_page_view(request):
     return HttpResponse("Hey, das hat doch gut funktioniert!")
 
 def single_gadget_view(request, gadget_id):
-    new_slug = slugify(gadgets[gadget_id]["name"])
-    new_url = reverse("gadget_slug_url", args=[new_slug])
-    return redirect(new_url)
+    if len(gadgets) > gadget_id:
+        new_slug = slugify(gadgets[gadget_id]["name"])
+        new_url = reverse("gadget_slug_url", args=[new_slug])
+        return redirect(new_url)
+    return HttpResponseNotFound("not found by me")
 
 def single_gadget_slug_view(request, gadget_slug):
-    gadget_match = {"result": "nothing"}
+    gadget_match = None
     
     for gadget in gadgets:
         if slugify(gadget["name"]) == gadget_slug:
             gadget_match = gadget
             
-    return JsonResponse(gadget_match)
+    if gadget_match:
+        return JsonResponse(gadget_match)
+    
+    raise Http404
