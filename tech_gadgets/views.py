f52de675f66db3f4da77970fc11fa173a68d5eda
Christoph <c.wagner.germany@gmail.com>
2024-10-06 13:54:24 +0200

    json2

--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import json
+# import json
+from django.http import JsonResponse
 from .dummy_data import gadgets
 
 # Create your views here.
@@ -9,4 +10,5 @@ def start_page_view(request):
     return HttpResponse("Hey, das hat doch gut funktioniert!")
 
 def single_gadget_view(request, gadget_id):
-    return HttpResponse(json.dumps(gadget_id))
+    # return HttpResponse(json.dumps(gadget_id))
+    return JsonResponse({"test": True})
