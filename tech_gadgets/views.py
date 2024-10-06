9e5ce3ab524140a7151575890984be35b892e900
Christoph <c.wagner.germany@gmail.com>
2024-10-06 13:30:49 +0200

    first commit

---
+++ tech_gadgets/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+from .dummy_data import gadgets
+
+# Create your views here.
+
+def start_page_view(request):
+    return HttpResponse("Hey, das hat doch gut funktioniert!")
+
+def single_gadget_view(request, gadget_id):
+    return HttpResponse(json.dumps(gadget_id))
