2ff7fdce136697f25e2efb978598b9a7c6559081
Christoph <c.wagner.germany@gmail.com>
2024-10-07 20:31:54 +0200

    red4

--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -6,6 +6,8 @@ from django.http import JsonResponse, HttpResponseNotFound, Http404
 from django.utils.text import slugify
 from django.urls import reverse
 
+from django.views import View
+
 from .dummy_data import gadgets
 
 # Create your views here.
@@ -13,23 +15,51 @@ from .dummy_data import gadgets
 def start_page_view(request):
     return HttpResponse("Hey, das hat doch gut funktioniert!")
 
-def single_gadget_view(request, gadget_id):
+def single_gadget_int_view(request, gadget_id):
     if len(gadgets) > gadget_id:
         new_slug = slugify(gadgets[gadget_id]["name"])
         new_url = reverse("gadget_slug_url", args=[new_slug])
         return redirect(new_url)
     return HttpResponseNotFound("not found by me")
 
-def single_gadget_slug_view(request, gadget_slug):
-    gadget_match = None
+class GadgetView(View):
+    def get(self, request, gadget_slug):
+        gadget_match = None
+        for gadget in gadgets:
+            if slugify(gadget["name"]) == gadget_slug:
+                gadget_match = gadget
+                
+        if gadget_match:
+            return JsonResponse(gadget_match)
+        raise Http404()
     
-    for gadget in gadgets:
-        if slugify(gadget["name"]) == gadget_slug:
-            gadget_match = gadget
-            
-    if gadget_match:
-        return JsonResponse(gadget_match)
-    raise Http404()
+    def post(self, request, *args, **kwargs):
+        try:
+            data = json.loads(request.body)
+            print(f"recieved data: {data["test"]}")
+            return JsonResponse({"response": "Success"})
+        except:
+            return JsonResponse({"response": "Failure"})
+
+def single_gadget_view(request, gadget_slug=""):
+    
+    if request.method == "GET":
+        gadget_match = None
+        for gadget in gadgets:
+            if slugify(gadget["name"]) == gadget_slug:
+                gadget_match = gadget
+                
+        if gadget_match:
+            return JsonResponse(gadget_match)
+        raise Http404()
+    
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            print(f"recieved data: {data["test"]}")
+            return JsonResponse({"response": "Success"})
+        except:
+            return JsonResponse({"response": "Failure"})
 
 def single_gadget_post_view(request):
     if request.method == "POST":
