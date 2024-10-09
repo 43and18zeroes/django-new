946f82328b1df42199d41cc5eebda2e09c496249
Christoph <c.wagner.germany@gmail.com>
2024-10-09 15:54:10 +0200

    red4

--- tech_gadgets/urls.py
+++ tech_gadgets/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('gadget/', GadgetView.as_view()),
     path('gadget/<int:gadget_id>', single_gadget_int_view),
     path('gadget/<slug:gadget_slug>', GadgetView.as_view(), name="gadget_slug_url"),
+    path('gadget/<path:any_path>', GadgetView.as_view(), name="gadget_slug_url"),
     path('manufacturer/<int:manufacturer_id>', single_manufacturer_int_view),
     path('manufacturer/<slug:manufacturer_slug>', ManufacturerView.as_view(), name="manufacturer_slug_url"),
     path('manufacturer/<path:any_path>', ManufacturerView.as_view(), name="manufacturer_slug_url")
